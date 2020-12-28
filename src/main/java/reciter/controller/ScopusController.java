7f4bed4660c653ad9576378fbd50d50bc63ced32
Sarbajit Dutta <sarbajitdutta35@gmail.com>
2020-12-28 10:40:53 -0500

    conform to sonarlint specifications

--- src/main/java/reciter/controller/ScopusController.java
+++ src/main/java/reciter/controller/ScopusController.java
@@ -1,35 +1,39 @@
 package reciter.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import reciter.model.scopus.ScopusArticle;
 import reciter.model.scopus.ScopusQuery;
 import reciter.scopus.retriever.ScopusArticleRetriever;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping("/scopus")
-@Api(value="ScopusController", description="Operations on querying the Scopus API.")
+@Api(value="ScopusController", tags = {"Querying Scopus with PMID, SCOPUS-ID or DOI"})
 public class ScopusController {
     private static final Logger slf4jLogger = LoggerFactory.getLogger(ScopusController.class);
 
-    @ApiOperation(value = "Querying Scopus with PMIDs.", response = List.class)
+    @ApiOperation(value = "Querying Scopus with PMID, SCOPUS-ID or DOI. Add type it only accepts PMID,SCOPUS-ID or DOI(case-insensitive)", response = List.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successfully retrieved list"),
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
-    @RequestMapping(value = "/query/", method = RequestMethod.POST, produces = "application/json")
+    @PostMapping(value = "/query/", produces = "application/json")
     @ResponseBody
     public ResponseEntity<List<ScopusArticle>> retrieve(@RequestBody ScopusQuery scopusQuery) {
         slf4jLogger.info("calling retrieve with pmids size=[" + scopusQuery.getQuery().size() + "]");
